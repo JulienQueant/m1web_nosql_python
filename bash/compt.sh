b53bf33ac8e89b2d4c23a5bc68cf584488eb65fc
JulienQueant <julien.queant@u-psud.fr>
2020-04-29 22:17:00 +0200

    Done first 4 bash exercices

---
+++ bash/compt.sh
@@ -0,0 +1,5 @@
+count=0
+while ((count < 5)); do
+    ((count++))
+    echo $count
+done
---
+++ bash/nameUser.sh
@@ -0,0 +1,9 @@
+currentUser=$USER
+currentDate=`date`
+
+echo The current user of this terminal is $currentUser
+echo Today\'s date is $currentDate
+
+echo Who am I talking to\?
+read userName
+echo Hey wassup $userName, how ya doin\?
